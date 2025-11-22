7c68e2539e16ed4ad88d1ef12e5ba808718a59ea
Mohd Sher Khan <sherkhan3570@gmail.com>
2025-11-22 16:00:44 +0530

    test for ui

--- App.tsx
+++ App.tsx
@@ -1,91 +1,136 @@
-import React, { useEffect, useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Text, View, StyleSheet, ActivityIndicator } from 'react-native';
-import SignUp from './src/views/SignUp';
-import SignIn from './src/views/SignIn';
-import MainPage from './src/views/MainPage';
-import auth from '@react-native-firebase/auth';
-import SignInWithPhone from './src/views/SignInWithPhone';
-
-const Stack = createNativeStackNavigator();
-
-function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-    </View>
-  );
-}
-
-function ProfileScreen({ route }: any) {
-  const id = route?.params?.id ?? {};
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
-      <Text style={styles.text}>ID: {id}</Text>
-    </View>
-  );
-}
-
-const linking = {
-  prefixes: ['testingNew://'],
-  config: {
-    screens: {
-      Home: 'Home',
-      Profile: 'Profile/:id',
-    },
-  },
-};
+import { Image, StyleSheet, Text, TextInput, View } from 'react-native';
+import React from 'react';
 
 const App = () => {
-  const [initial, setInitial] = useState(true);
-  const [user, setUser] = useState(null);
-
-  const information = (user: any) => {
-    setUser(user);
-    setInitial(false);
-  };
-
-  useEffect(() => {
-    const subscriber = auth().onAuthStateChanged(information);
-    return subscriber;
-  }, []);
-
-  console.log('checkUser---', user);
-  if (initial) {
-    return <ActivityIndicator size="large" />;
-  }
   return (
-    <NavigationContainer linking={linking}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {user ? (
-          <Stack.Screen name="Main" component={MainPage} />
-        ) : (
-          <>
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="SignIn" component={SignIn} />
-            <Stack.Screen name="Phone" component={SignInWithPhone} />
-          </>
-        )}
+    <View style={{ flex: 1, backgroundColor: 'white' }}>
+      <View
+        style={{
+          backgroundColor: 'white',
+          flexDirection: 'row',
+          borderBottomColor: 'grey',
+          borderBottomWidth: 0.3,
+          justifyContent: 'space-between',
+          top: 20,
+          marginBottom: 30,
+        }}
+      >
+        <View
+          style={{
+            width: 36,
+            height: 36,
+            backgroundColor: 'grey',
+            borderRadius: 50,
+            left: 10,
+          }}
+        ></View>
+        <View
+          style={{
+            width: 250,
+            height: 38,
+            borderRadius: 10,
+            backgroundColor: 'white',
+            borderColor: 'grey',
+            borderWidth: 1,
+            marginBottom: 10,
+          }}
+        >
+          <TextInput placeholder="Serach" />
+        </View>
+        <View style={{ right: 10, top: 5 }}>
+          <Text>Chat</Text>
+        </View>
+      </View>
 
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <View>
+        <View
+          style={{
+            backgroundColor: 'white',
+            flexDirection: 'row',
+            borderBottomColor: 'grey',
+            borderBottomWidth: 0.3,
+            justifyContent: 'space-between',
+            borderTopColor: 'grey',
+            height: 30,
+          }}
+        >
+          <Text style={{ left: 10, fontWeight: 'bold' }}>Suggested</Text>
+          <View
+            style={{ flexDirection: 'row', justifyContent: 'space-around' }}
+          >
+            <Text
+              style={{ color: 'Black', right: 50, fontSize: 20, bottom: 10 }}
+            >
+              ...
+            </Text>
+            <Text
+              style={{
+                color: 'black',
+                right: 20,
+                fontSize: 18,
+                fontWeight: 'bold',
+              }}
+            >
+              X
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View>
+        <View
+          style={{
+            backgroundColor: 'white',
+            flexDirection: 'row',
+            marginVertical: 20,
+          }}
+        >
+          <View
+            style={{
+              width: 50,
+              height: 50,
+              backgroundColor: 'grey',
+              borderRadius: 50,
+              left: 5,
+            }}
+          />
+          <View
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
+          >
+            <View style={{ marginLeft: 10 }}>
+              <Text
+                style={{ color: 'black', fontSize: 18, fontWeight: 'bold' }}
+              >
+                Ashish Singhal
+              </Text>
+              <Text style={{ color: 'grey', fontSize: 14 }}>
+                Co-founder co-switch
+              </Text>
+              <Text style={{ color: 'blue', fontSize: 12 }}>
+                View My Newsletter
+              </Text>
+              <Text style={{ color: 'grey', fontSize: 12 }}>2d.</Text>
+            </View>
+            <View>
+              <Text style={{ color: 'blue', fontSize: 16, left: 80,fontWeight:"bold" }}>
+                + Follow
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ padding: 10 }}>
+          <Text>
+            fdfjknaljsdjadscadhfhcjsdfcslkdhahdkjchdhfsgdfghgdfhjksdhjfhchdfjhcsdkfhskfg
+          </Text>
+          <View style={{ padding: 20, alignItems: 'center' }}>
+            <Image
+              style={{ width: 300, height: 400, resizeMode: 'cover' }}
+              source={{ uri: 'https://picsum.photos/536/354' }}
+            />
+          </View>
+        </View>
+      </View>
+    </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
